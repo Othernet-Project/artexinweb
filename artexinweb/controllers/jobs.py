f9eea1eb709d9f43eeae670cb27eceb9b0a0966b
Andrean Franc <andrean.franc@gmail.com>
2015-02-26 22:08:58 +0100

    use bottle prefixed decorators to allow easy mocking in unittests

--- artexinweb/controllers/jobs.py
+++ artexinweb/controllers/jobs.py
@@ -2,30 +2,24 @@
 import os
 import uuid
 
-from bottle import (request,
-                    route,
-                    get,
-                    post,
-                    jinja2_view,
-                    jinja2_template,
-                    redirect)
+import bottle
 
 from artexinweb import settings
 from artexinweb.forms import FetchableJobForm, StandaloneJobForm
 from artexinweb.models import Job
 
 
-@get('/')
-@jinja2_view('job_dashboard.html')
+@bottle.get('/')
+@bottle.jinja2_view('job_dashboard.html')
 def job_dashboard():
     return {'erred_job_count': Job.objects.filter(status=Job.ERRED).count(),
             'erred_status': Job.ERRED}
 
 
-@get('/jobs/')
-@jinja2_view('job_list.html')
+@bottle.get('/jobs/')
+@bottle.jinja2_view('job_list.html')
 def jobs_list():
-    status = request.query.get('status')
+    status = bottle.request.query.get('status')
     if status:
         job_list = Job.objects.filter(status=status)
     else:
@@ -49,7 +43,7 @@ class JobController(object):
                    targets=form.urls.data,
                    extract=form.extract.data,
                    javascript=form.javascript.data)
-        return redirect('/jobs/')
+        return bottle.redirect('/jobs/')
 
     @classmethod
     def standalone(cls, job_type, form):
@@ -59,69 +53,72 @@ class JobController(object):
 
         os.makedirs(upload_path)
 
-        for uploaded_file in request.files.getlist('files'):
+        for uploaded_file in bottle.request.files.getlist('files'):
             uploaded_file.save(upload_path)
 
         Job.create(targets=[upload_path],
                    job_type=job_type,
                    origin=form.origin.data)
-        return redirect('/jobs/')
+        return bottle.redirect('/jobs/')
 
 
-@post('/jobs/')
+@bottle.post('/jobs/')
 def jobs_create():
-    job_type = request.forms.get('type')
+    job_type = bottle.request.forms.get('type')
 
     if Job.is_valid_type(job_type):
         form_cls = JobController.forms[job_type]
-        form_data = request.forms.decode()
-        form_data.update(request.files)
+        form_data = bottle.request.forms.decode()
+        form_data.update(bottle.request.files)
         form = form_cls(form_data)
 
         if form.validate():
             return getattr(JobController, job_type.lower())(job_type, form)
 
         template_name = 'job_{0}.html'.format(job_type.lower())
-        return jinja2_template(template_name, form=form, job_type=job_type)
+        return bottle.jinja2_template(template_name,
+                                      form=form,
+                                      job_type=job_type)
 
-    return jinja2_template('job_wizard.html')
+    return bottle.jinja2_template('job_wizard.html')
 
 
-@get('/jobs/actions/new/')
+@bottle.get('/jobs/actions/new/')
 def jobs_new():
-    job_type = request.query.get('type')
+    job_type = bottle.request.query.get('type')
     if Job.is_valid_type(job_type):
         form_cls = JobController.forms[job_type]
         form = form_cls()
 
-        return jinja2_template('job_{0}.html'.format(job_type.lower()),
-                               form=form,
-                               job_type=job_type)
+        return bottle.jinja2_template('job_{0}.html'.format(job_type.lower()),
+                                      form=form,
+                                      job_type=job_type)
 
-    return jinja2_template('job_wizard.html')
+    return bottle.jinja2_template('job_wizard.html')
 
 
-@route('/jobs/<job_id:re:[a-zA-Z0-9]+>/actions/retry/', method=['GET', 'POST'])
+@bottle.route('/jobs/<job_id:re:[a-zA-Z0-9]+>/actions/retry/',
+              method=['GET', 'POST'])
 def jobs_retry(job_id):
-    if request.method == 'POST':
+    if bottle.request.method == 'POST':
         job = Job.objects.get(job_id=job_id)
         if not job.is_finished:
             job.retry()
 
-        return redirect('/jobs/')
+        return bottle.redirect('/jobs/')
 
-    return jinja2_template('job_retry.html', job_id=job_id)
+    return bottle.jinja2_template('job_retry.html', job_id=job_id)
 
 
-@get('/jobs/<job_id:re:[a-zA-Z0-9]+>/')
-@jinja2_view('job_details.html')
+@bottle.get('/jobs/<job_id:re:[a-zA-Z0-9]+>/')
+@bottle.jinja2_view('job_details.html')
 def jobs_details(job_id):
     job = Job.objects.get(job_id=job_id)
     return {'job': job}
 
 
-@get('/jobs/<job_id:re:[a-zA-Z0-9]+>/tasks/')
-@jinja2_view('task_list.html')
+@bottle.get('/jobs/<job_id:re:[a-zA-Z0-9]+>/tasks/')
+@bottle.jinja2_view('task_list.html')
 def task_list(job_id):
     job = Job.objects.get(job_id=job_id)
     return {'task_list': job.tasks, 'job_id': job_id}
