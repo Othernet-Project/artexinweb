b9b5221f7ab9592e36121e5a7bc9f5fa971fda5c
Andrean Franc <andrean.franc@gmail.com>
2015-03-03 19:46:14 +0100

    add babel req and a utility function to collect all available languages

--- artexinweb/utils.py
+++ artexinweb/utils.py
@@ -3,6 +3,8 @@ import hashlib
 import os
 import pkgutil
 
+import babel
+
 
 def discover(package):
     modules = pkgutil.iter_modules(package.__path__)
@@ -22,3 +24,24 @@ def hash_data(*args):
 
 def get_extension(filepath):
     return os.path.splitext(filepath)[-1].strip(".").lower()
+
+
+def collect_locales():
+    """Collects all the languages supported by Babel.
+
+    :returns:  list of (language_code, language_name) tuples"""
+    languages = []
+
+    for lang_code in babel.localedata.locale_identifiers():
+        locale = babel.Locale(lang_code)
+        if locale.english_name:
+            if (not locale.display_name or
+                    locale.display_name == locale.english_name):
+                label = locale.english_name
+            else:
+                label = '{0} ({1})'.format(locale.display_name,
+                                           locale.english_name)
+
+            languages.append((lang_code, label))
+
+    return languages
