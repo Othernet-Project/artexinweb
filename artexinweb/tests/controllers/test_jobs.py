9a2ca08b56f0682b9b89f1d9bd1cc70c0c9a302c
Andrean Franc <andrean.franc@gmail.com>
2015-03-11 22:09:33 +0100

    add controller unittests(wip)

--- artexinweb/tests/controllers/test_jobs.py
+++ artexinweb/tests/controllers/test_jobs.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from unittest import mock
 
-import artexinweb.controllers.jobs
+
+def pass_through(template_name):
+    def _pass_through(func):
+        def __pass_through(*args, **kwargs):
+            return func(*args, **kwargs)
+        return __pass_through
+    return _pass_through
 
 
 class TestJobControllers(object):
@@ -9,23 +15,27 @@ class TestJobControllers(object):
     @mock.patch('bottle.jinja2_view')
     @mock.patch('artexinweb.models.jobs.Job.objects')
     def test_task_list(self, job_objects, jinja2_view):
-
-        def pass_through(template_name):
-            def _pass_through(func):
-                def __pass_through(*args, **kwargs):
-                    result = func(*args, **kwargs)
-                    return result
-                return __pass_through
-            return _pass_through
-
         jinja2_view.side_effect = pass_through
-
-        # from artexinweb.controllers import jobs as job_controllers
 
         mocked_job = mock.Mock()
         mocked_job.tasks = []
         job_objects.get.return_value = mocked_job
 
-        result = artexinweb.controllers.jobs.task_list('job_id')
+        from artexinweb.controllers.jobs import task_list
+        result = task_list('job_id')
 
-        # assert result == {'task_list': [], 'job_id': 'job_id'}
+        assert result == {'task_list': [], 'job_id': 'job_id'}
+
+    @mock.patch('bottle.jinja2_view')
+    @mock.patch('artexinweb.models.jobs.Job.objects')
+    def test_job_details(self, job_objects, jinja2_view):
+        jinja2_view.side_effect = pass_through
+
+        mocked_job = mock.Mock()
+        mocked_job.tasks = []
+        job_objects.get.return_value = mocked_job
+
+        from artexinweb.controllers.jobs import jobs_details
+        result = jobs_details('job_id')
+
+        assert result == {'job': mocked_job}
